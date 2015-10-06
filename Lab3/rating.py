e209a5a38727ed5502f51fbf15dcb6f0a189d58e
Banson Chen <epyonchen@users.noreply.github.com>
2015-10-05 22:16:33 -0400

    Update and rename rating to rating.py

--- Lab3/rating.py
+++ Lab3/rating.py
@@ -28,9 +28,8 @@ with con:
 	ratingdb = con.cursor()
 	ratingdb.execute("DROP TABLE IF EXISTS MoviesInfo") 
 	ratingdb.execute("CREATE TABLE MoviesInfo(name TEXT, rate REAL, vote INT, year TEXT, PB INT, DG INT, WG INT)")
-	count0 = 0
+	count = 0
 	for line in response: #  Get vote, rate, name and year of movies
-
 	   words = re.sub(' +',' ',line.strip()) #  This is a string!
 	   save = words.split(" ",3) #  Split vote, rate, name and year
 	   vote = int(save[1].replace("'", "")) #  Get votes
@@ -38,14 +37,14 @@ with con:
 	   nameANDdate = save[3].split(" (",1) #  Only part of  names have quotation, so we can not split name with quotation
 	   name = nameANDdate[0].replace('"', '') #  Get names
 	   print name
-	   year = nameANDdate[1] #  Get years
-	   year = re.sub("\D","",year) 
+	   year = nameANDdate[1].replace("?","0") #  Get years
+	   year = int(re.sub("\D","",year) )
 	   print year
 	   count = count +1
 	   print count
 	   #if count>100: #  Limit numbers to store
 	     #  break
-	   insertStatement = 'INSERT INTO MoviesInfo(name, rate, vote, year) VALUES("%s",%f,%d,"%s")' % (name,rate,vote,year) #  Add movies' names and ratings into database
+	   insertStatement = 'INSERT INTO MoviesInfo(name, rate, vote, year) VALUES("%s",%f,%d,%d)' % (name,rate,vote,year) #  Add movies' names and ratings into database
 	   ratingdb.execute(insertStatement)
 		
 	## NEEDED, if not, database does not update

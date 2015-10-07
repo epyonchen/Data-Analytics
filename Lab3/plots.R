0e010dc88f728d5a4ab72107e5522ac6cd3b9012
Banson Chen <epyonchen@users.noreply.github.com>
2015-10-07 18:03:16 -0400

    Update plots.R

--- Lab3/plots.R
+++ Lab3/plots.R
@@ -4,22 +4,22 @@ library("RSQLite")
 sqlite = dbDriver("SQLite")
 moviesDB = dbConnect(sqlite,"movies.db") #  Connect to database
 
-query = dbSendQuery(moviesDB, "SELECT rate,PB FROM MoviesInfo" ) #  Get each rate
+query = dbSendQuery(moviesDB, "SELECT rate FROM MoviesInfo" ) #  Get each rate
 rate = dbFetch(query, n = -1)
 rate = rate[[1]]
 query = dbSendQuery(moviesDB, "SELECT year FROM MoviesInfo" ) #  Get each year
 year = dbFetch(query, n = -1)
 year = year[[1]]
-query = dbSendQuery(moviesDB, "SELECT year FROM MoviesInfo" ) #  Get each vote
+query = dbSendQuery(moviesDB, "SELECT vote FROM MoviesInfo" ) #  Get each vote
 vote = dbFetch(query, n = -1)
 vote = vote[[1]]
-query = dbSendQuery(moviesDB, "SELECT year FROM MoviesInfo" ) #  Get each Production Budget
+query = dbSendQuery(moviesDB, "SELECT PB FROM MoviesInfo" ) #  Get each Production Budget
 PB = dbFetch(query, n = -1)
 PB = PB[[1]]
-query = dbSendQuery(moviesDB, "SELECT year FROM MoviesInfo" ) #  Get each Worldwide Gross
+query = dbSendQuery(moviesDB, "SELECT WG FROM MoviesInfo" ) #  Get each Worldwide Gross
 WG = dbFetch(query, n = -1)
 WG = WG[[1]]
-query = dbSendQuery(moviesDB, "SELECT year FROM MoviesInfo" ) #  Get each Democratic Gross
+query = dbSendQuery(moviesDB, "SELECT DG FROM MoviesInfo" ) #  Get each Democratic Gross
 DG = dbFetch(query, n = -1)
 DG = DG[[1]]
 query = dbSendQuery(moviesDB, "SELECT * FROM MoviesInfo" ) #  Get all Information
@@ -27,6 +27,7 @@ moviesinfo = dbFetch(query, n = -1)
 query = dbSendQuery(moviesDB, "SELECT * FROM MoviesInfo Where year >= 2010" ) #  Get movies after 2010
 movies2010s = dbFetch(query, n = -1)
 movies2010s[4] = 2010
+print(movies2010s[1])
 query = dbSendQuery(moviesDB, "SELECT * FROM MoviesInfo Where year >= 2000 AND year < 2010" ) #  Get movies between 2000 and 2010
 movies2000s = dbFetch(query, n = -1)
 movies2000s[4] = 2000
@@ -60,12 +61,12 @@ movies1910s[4] = 1910
 moviesdecades = rbind(movies1910s,movies1920s,movies1930s,movies1940s,movies1950s,movies1960s,movies1970s,movies1980s,movies1990s,movies2000s,movies2010s) # Rearrange movies in dacades
 dbClearResult(query)
 
-#plot(year, rate, main="Scatterplot Example", xlab="Rate ", ylab="Production Budget ", pch=19) #  画散点图
-#abline(lm(rate~year), col="red") # regression line (y~x) #  根据散点图线性拟合
-#cor(rate, PB) #  计算相关系数
-
+plot(PB, DG, main="Production Budget", xlab="Production Budget", ylab="Domestic Gross", pch=19) #  画散点图
+abline(lm(DG~PB ), col="red") # regression line (y~x) #  根据散点图线性回归
+cor(DG, PB) #  计算相关系数
+#boxplot(PB~year,data=moviesdecades, main="Car Milage Data", xlab="Number of Cylinders", ylab="Miles Per Gallon")
 library(ggplot2)
-#ggplot(moviesinfo,aes(x = rate))+ geom_histogram(aes(fill = ..count..)) #  x的总量直方图
+#gplot(moviesinfo,aes(x = PB))+ geom_histogram(aes(fill = ..count..)) #  x的总量直方图
 #ggplot(moviesdecades, aes(factor(year), PB)) + geom_violin() #  PB在不同year上的分布
 #ggplot(moviesinfo, aes(factor(round(rate, digits = 0)), vote)) + geom_violin() #  没懂什么意思
 

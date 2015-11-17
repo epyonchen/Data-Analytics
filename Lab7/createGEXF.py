2554e2383d4a787ae0d6f07aea1ec66f536a479f
Banson Chen <epyonchen@users.noreply.github.com>
2015-11-17 00:39:23 -0500

    Update and rename rewriteTXT.py to createGEXF.py
    Don't need to set txt default every run again.

--- Lab7/createGEXF.py
+++ Lab7/createGEXF.py
@@ -1,4 +1,3 @@
-# Set twitter.txt as default before run this code
 import os
 import sqlite3 as lite
 
@@ -6,8 +5,8 @@ fileline = list()
 directory = "D:/Workshop/homework/Data Analytics/Lab 7/"
 if not os.path.exists(directory):# If directory does not exist, creates items
 	os.makedirs(directory)
-directoryForTXT = directory + "twitter.txt"# File name
-fileread = open(directoryForTXT,"r")# Open file
+directoryForTXT = directory + "twitter.txt"# TXT name
+fileread = open(directoryForTXT,"r")# Open TXT
 for line in fileread:# Read file in each line, and append into fileline list
     fileline.append(line)
 
@@ -15,6 +14,8 @@ con = None
 directoryForDB = directory + "twitter.db"
 con = lite.connect(directoryForDB)# Connect database
 
+countnode = 0
+countlink = 0
 getDB = con.cursor()
 getDB.execute("SELECT * FROM node")# Get nodes from database
 nodes = getDB.fetchall()
@@ -22,9 +23,13 @@ getDB.execute("SELECT * FROM link")# Get links from database
 links = getDB.fetchall()
 for node in nodes:# Insert nodes into fileline list
     fileline.insert(8,'			<node id="%s" label="%s"/>\n'%(node[0],node[0]))
+    countnode= countnode + 1
 for link in links:# Insert links into fileline list
-    fileline.insert(len(fileline)-3,'			<edge source="%s" target="%s"/>\n'%(link[0],link[1]))    
+    fileline.insert(len(fileline)-3,'			<edge source="%s" target="%s"/>\n'%(link[0],link[1]))
+    countlink = countlink + 1    
 
-fileupdate = open(directoryForTXT,"w")# Open file
+directoryForGEXF = directory + "twitter.GEXF"# GEXF name
+fileupdate = open(directoryForGEXF,"w")# Open GEXF
 fileupdate.writelines(fileline)# Rewrite file base on fileline list(with nodes and links)
 fileupdate.close()
+print "Nodes: %s. Links: %s"%(countnode, countlink)

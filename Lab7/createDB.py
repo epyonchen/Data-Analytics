66effd3a717a2d41296ceeb435f13cf4d5044155
Banson Chen <epyonchen@users.noreply.github.com>
2015-11-18 17:10:56 -0500

    Update createDB.py
    Add time attribute

--- Lab7/createDB.py
+++ Lab7/createDB.py
@@ -12,7 +12,7 @@ con = lite.connect(directoryForDB)
 with con:# Create database
 	pop = con.cursor()
 	pop.execute("DROP TABLE IF EXISTS node") 
-	pop.execute("CREATE TABLE node(nodeid TEXT, PRIMARY KEY(nodeid))")# Set nodeid as Primary key to avoid dupicate
+	pop.execute("CREATE TABLE node(nodeid TEXT, time TEXT,PRIMARY KEY(nodeid))")# Set nodeid as Primary key to avoid dupicate
 	pop.execute("DROP TABLE IF EXISTS link") 
-	pop.execute("CREATE TABLE link(nodeid TEXT, linkto TEXT, PRIMARY KEY(nodeid, linkto))")# Set nodeid, linkto as Primary key, one link appears only once
+	pop.execute("CREATE TABLE link(nodeid TEXT, linkto TEXT, time TEXT, PRIMARY KEY(nodeid, linkto))")# Set nodeid, linkto as Primary key, one link appears only once
 	con.commit()
